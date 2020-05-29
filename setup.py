f9ca1e8a78e79bc6717265eed02d9d5cec0f87bf
Fufu Fang <fangfufu2003@gmail.com>
2020-05-29 14:05:13 +0100

    updated package description

--- setup.py
+++ setup.py
@@ -38,13 +38,12 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.1',  # Required
+    version='0.0.2',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#summary
-    description='Count the number of regex appearances within a subreddit \
-    between a certain period of time', # Optional
+    description='Count the number of regex matches within a subreddit', # Optional
 
     # This is an optional longer description of your project that represents
     # the body of text which users will see when they visit PyPI.
